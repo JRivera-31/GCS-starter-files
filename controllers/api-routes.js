4df8d0a1159d8480c797d07ecedf610003e8301e
Josh R <jriver35@uncc.edu>
2020-06-22 15:57:34 -0400

    First commit

---
+++ controllers/api-routes.js
@@ -0,0 +1,11 @@
+const db = require("../models")
+const Multer = require("multer")
+const { Storage } = require("@google-cloud/storage")
+const uuid = require("uuid")
+
+// env
+require("dotenv").config()
+
+module.exports = function (app) {
+
+}
